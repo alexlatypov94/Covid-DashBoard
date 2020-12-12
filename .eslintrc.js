de223f4ddff55e5cda6e668b2333d56f8e2a57e2
Aliaksei <alex.latypov94@gmail.com>
2020-12-12 15:14:56 +0300

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-        "extends": [
-            "airbnb-base",
-            "prettier"
-        ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ["airbnb-base", "prettier"],
+    parser: "babel-eslint",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
     },
-    "rules": {
+    rules: {
         "import/prefer-default-export": "off",
         "import/export": "off",
         "class-methods-use-this": "off",
