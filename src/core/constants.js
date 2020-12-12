15410d8e637dd73d12ced0ded8bef148f158368f
Aliaksei <alex.latypov94@gmail.com>
2020-12-12 13:52:04 +0300

    feat: add constants

---
+++ src/core/constants.js
@@ -0,0 +1,10 @@
+export const API_URL = "https://api.covid19api.com";
+
+export const URL = {
+    SUMMARY: "summary",
+    CONFIRMED: "confirmed",
+    COUNTRY: "country",
+    COUNTRIES: "countries",
+    DAYONE: "dayone",
+    STATUS: "status"
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "./assets/css/style.scss";
-import { BaseMarkup, getJSON } from "./components/index";
+import { BaseMarkup } from "./components/index";
 
 const markup = new BaseMarkup();
 
 markup.init();
-
-const firstAPI = "https://api.covid19api.com/summary";
-
-getJSON(firstAPI).then((data) => console.log(data));
