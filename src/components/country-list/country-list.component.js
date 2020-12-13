fc434afeac31a23be1f3d20256f08cbb2ccfb24c
Nimlu <snesterov@gmail.com>
2020-12-13 17:55:22 +0300

    feat: add search,some styles, custom scroll

--- src/components/country-list/country-list.component.js
+++ src/components/country-list/country-list.component.js
@@ -1,49 +1,46 @@
 import { CovidDashboardService } from "../../core/index";
-import { Chart} from "./chart/index";
+import { Chart } from "./chart/index";
 import { countryListTemplate } from "./country-list.template";
 
 export class CountryList {
-	
-
     constructor() {
         this.chart = new Chart();
-				this.service = new CovidDashboardService();
-				// this.countriesArray=null;
-
+        this.service = new CovidDashboardService();
+        // this.countriesArray=null;
     }
-
 
     init() {
         // const value = "belarus";
-				// this.service.getDayOne(value).then((data) => this.viewData(data));
-				const list = document.querySelector('.country-list');
-				list.insertAdjacentHTML("beforeend", countryListTemplate);
-				this.chart.init();
-			this.service.getSummary().then((items) => this.appendFlags(items));
-		  // this.service.getFlagsAndPopulations().then((data) => this.storeFlags(data));
+        // this.service.getDayOne(value).then((data) => this.viewData(data));
+        const list = document.querySelector(".country-list");
+        list.insertAdjacentHTML("beforeend", countryListTemplate);
+        this.chart.init();
+        this.service.getSummary().then((items) => this.appendFlags(items));
+        // this.service.getFlagsAndPopulations().then((data) => this.storeFlags(data));
     }
 
     viewData(data) {
-				this.chart.log(data);
-				this.chart.render(data);
-		}
+        this.chart.log(data);
+        this.chart.render(data);
 
-		appendFlags(data){
-			const countries = data.countries.sort((a, b) => (b.country < a.country ? 1 : -1))
-			this.service.getFlagsAndPopulations().then((flags) => {
+        const input = document.querySelector(".country-input");
+        input.addEventListener("keyup", () => {
+            this.chart.serch();
+        });
+    }
 
-countries.forEach((el) => {
-	
-	flags.forEach((flag)=>{
+    appendFlags(data) {
+        const countries = data.countries.sort((a, b) => (b.country < a.country ? 1 : -1));
+        this.service.getFlagsAndPopulations().then((flags) => {
+            countries.forEach((el) => {
+                flags.forEach((flag) => {
+                    if (el.country === flag.name) {
+                        el.flag = flag.flag;
+                    }
+                });
+            });
 
-		if(el.country===flag.name){
-			el.flag = flag.flag;
-		}
-
-	})	
-});
-
-this.viewData(countries);
-			})
-	}
+            this.viewData(countries);
+        });
+    }
 }
