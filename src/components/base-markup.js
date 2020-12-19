9609d8a4efe55b446b57814dbade41915ee7ebb1
Aliaksei <67701828+alexlatypov94@users.noreply.github.com>
2020-12-19 16:37:48 +0200

    feat: added footer & header (#22)

--- src/components/base-markup.js
+++ src/components/base-markup.js
@@ -4,7 +4,8 @@ export class BaseMarkup {
             "afterbegin",
             `
         <header>
-        
+            <h1 class = "header-title">Covid-19 dashboard</h1>
+            <img src = "assets/img/virus.svg" alt = "" class = "header-logo">
         </header>
         <main>
         <div class = "wrapper">
@@ -19,7 +20,16 @@ export class BaseMarkup {
         </div>
         </main>
         <footer>
-        
+            <div class = "footer-dev-ops">
+                <ul class = "dev-ops-list">
+                    <li class = "dev-ops-list-person">Developers: </li>
+                    <li class = "dev-ops-list-person"><a href = "https://github.com/alexlatypov94" class = "dev-ops-ref"><img class = "footer-github-logo" src = "assets/img/github.svg"><p>Aliaksei Latypay</p></a></li>
+                    <li class = "dev-ops-list-person"><a href = "https://github.com/dubichtonya" class = "dev-ops-ref"><img class = "footer-github-logo" src = "assets/img/github.svg"><p>Antonina Dubich</p></a></li>
+                    <li class = "dev-ops-list-person"><a href = "https://github.com/nimlu-bot" class = "dev-ops-ref"><img class = "footer-github-logo" src = "assets/img/github.svg"><p>Sergey Nesterov</p></a></li>
+                </ul>
+                <h3 class = "footer-create-year">2020</h3>
+                <a href = "https://rs.school/js/" class = "footer-rs-ref"><img class = "footer-rs-logo" src = "assets/img/rs-school-js.svg"></a>
+            </div>
         </footer>
         
         `
