f7471a374a0b60996a1ef759661a720d10eed859
Nimlu <snesterov@gmail.com>
2020-12-23 19:20:50 +0300

    fix: fix test for map

--- src/components/map/map.component.spec.js
+++ src/components/map/map.component.spec.js
@@ -5,9 +5,10 @@ const map = new MapCovied();
 describe("MapCovied ", () => {
     test("getPopupMapName shoud be return correct names", () => {
         expect(map.getPopupMapName("totalCases")).toBe("Total cases");
-        expect(map.getPopupMapName("active")).toBe("Active");
-        expect(map.getPopupMapName("recovered")).toBe("Total recovered");
-        expect(map.getPopupMapName("death")).toBe("Total deaths");
-        expect(map.getPopupMapName()).toBe("Total cases");
+        expect(map.getPopupMapName("totalDeaths")).toBe("Total deaths");
+        expect(map.getPopupMapName("totalRecovered")).toBe("Total recovered");
+        expect(map.getPopupMapName("todayCases")).toBe("Today Cases");
+        expect(map.getPopupMapName("todayDeaths")).toBe("Today Deaths");
+        expect(map.getPopupMapName()).toBe("Today Recovered");
     });
 });
